6e216bcceffa1a5d4b5c9ade6bdc838f17118330
Ravil Berishev <quadraticteam@gmail.com>
2022-05-20 06:51:20 +0300

    !

--- components/simpleBlock/index.tsx
+++ components/simpleBlock/index.tsx
@@ -6,8 +6,7 @@ export interface SimpleBlockProps {}
 export const SimpleBlock: React.FC<PropsWithChildren<SimpleBlockProps>> = ({
   children,
 }) => {
-  const background = useColorModeValue("gray.50", "gray.900");
-  const borderColor = useColorModeValue("gray.50", "gray.100");
+  const background = useColorModeValue("white.50", "whiteAlpha.50");
   const boxShadowColor = useColorModeValue(
     "--chakra-colors-blue-200",
     "--chakra-colors-pink-200"
@@ -17,7 +16,6 @@ export const SimpleBlock: React.FC<PropsWithChildren<SimpleBlockProps>> = ({
       py={{ base: 6 }}
       px={{ base: 6 }}
       background={background}
-      borderColor={borderColor}
       backdropFilter="blur(100px)"
       borderRadius={{ base: "base", lg: "lg" }}
       transition="all 0.6s ease 0s"
